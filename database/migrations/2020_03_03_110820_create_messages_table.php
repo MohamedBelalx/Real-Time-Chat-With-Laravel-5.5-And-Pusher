41668268a473dfd0943bc6cd6f7b723e6cf8a30a
MohamedBelalx <hero.armna@gmail.com>
2020-03-03 13:31:28 +0200

    add migrations files

--- database/migrations/2020_03_03_110820_create_messages_table.php
+++ database/migrations/2020_03_03_110820_create_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMessagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('avatar')->default('avatar.jpg');
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('from');
+            $table->bigInteger('to');
+            $table->unsignedInteger('user_id');
+            $table->text('message');
+            $table->tinyInteger('is_read');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('messages');
     }
 }
